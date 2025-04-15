368f968b542e67b2e98fd149c58073c3cc77cb16
Prajaaa08 <lestariaagustina@gmail.com>
2025-04-15 10:54:14 +0700

    Menambahkan auth route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
@@ -11,3 +12,7 @@ use App\Http\Controllers\PostController;
 
 
 Route::apiResource('posts', PostController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
